560c063131b2debb63960999ead73fec2b6d23da
Chuck <cthier@gmail.com>
2012-06-08 11:21:04 -0500

    first commit

---
+++ pelican.conf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Chuck Thier"
+SITENAME = u"Chuck Thier"
+SITEURL = 'http://creiht.com/'
+
+THEME = 'theme'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+TWITTER_USERNAME = 'creiht'
+DISQUS_SITENAME = 'creiht'
+
+# Blogroll
+#LINKS =  (
+#         )
+
+# Social widget
+SOCIAL = (
+        ('Twitter', 'https://twitter.com/#!/creiht'),
+        ('500px', 'http://500px.com/creiht'),
+        ('github', 'https://github.com/creiht'),
+        ('Google+', 'https://plus.google.com/u/0/101148193646785300221'),
+        ('Facebook', 'http://www.facebook.com/cthier'),
+        ('Linkedin',  'http://www.linkedin.com/in/cthier'),
+         )
+
+DEFAULT_PAGINATION = 10
---
+++ src/general/hello.rst
@@ -0,0 +1,15 @@
+Hello World
+###########
+
+:date: 2012-06-08 10:30
+:tags: cloudfiles, blog, swift, openstack
+
+Welcome to my new blog thanks to:
+
+*  `Pelican <http://pelican.notmyidea.org>`_
+*  `Rackspace CloudFiles with Akamai CDN <http://www.rackspace.com/cloud/cloud_hosting_products/files/>`_
+*  `Disqus <http://disqus.com>`_
+*  `Github <https://github.com/creiht/creiht.com>`_
+*  `Swiftly <https://github.com/gholt/swiftly>`_
+
+And more specifically, the new `static web <http://docs.rackspace.com/files/api/v1/cf-devguide/content/Create_Static_Website-dle4000.html>`_ features of CloudFiles.  Very cool stuff!
